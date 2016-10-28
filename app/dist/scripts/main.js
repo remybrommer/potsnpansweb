11d0c6a7b11859dac6c2b5511665066356d39a87
Adrian Remy Brommer <Remy@Adrians-MacBook-Pro.local>
2016-10-28 11:18:07 -0400

    started work page

--- app/dist/scripts/main.js
+++ app/dist/scripts/main.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
         var bottom_of_object = $(this).offset().top + $(this).outerHeight();
         var bottom_of_window = $(window).scrollTop() + $(window).height();
         if( bottom_of_window > bottom_of_object ){
-          $(this).animate({'opacity':'1'},500);
+          $(this).animate({'opacity':'1'},300);
         }
       });
     });
